7b2a5524d9687b1b0c7b9099cff719bf708f91cc
HUNET-IN\shchoi <shchoi@hunet.co.kr>
2022-08-02 14:59:25 +0900

    expose app component to use from outer projects

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          It is a Shared React Component!
         </a>
       </header>
     </div>
